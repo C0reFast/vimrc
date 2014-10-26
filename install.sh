0061d2e58ebf05a49bd4d941640a7541a8582484
C0reFast <topchenfu@qq.com>
2014-10-26 18:38:15 +0800

    backup old config before install

--- install.sh
+++ install.sh
@@ -3,6 +3,11 @@
 
 CURRENT_DIR=`pwd`
 
+echo "Backing up current vim config"
+today=`date +%Y%m%d`
+for i in $HOME/.vim $HOME/.vimrc $HOME/.gvimrc $HOME/.vimrc.bundles; do [ -e $i ] && [ ! -L $i ] && mv $i $i.$today; done
+for i in $HOME/.vim $HOME/.vimrc $HOME/.gvimrc $HOME/.vimrc.bundles; do [ -L $i ] && unlink $i ; done
+
 cp $CURRENT_DIR/vimrc $HOME/.vimrc
 
 if [ ! -e $HOME/.vim/bundle/Vundle.vim ]; then
